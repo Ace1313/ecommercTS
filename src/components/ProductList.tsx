ddcc4ccee5fb811d1512ae0b3f1ab1d0d7a82091
Ace1313 <alex.cadjo@hotmail.com>
2022-02-09 15:08:28 +0100

    filter

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -2,31 +2,86 @@ import { ProductItem } from '../interface/ecomerce.interface';
 import styled from 'styled-components';
 import { useContext } from 'react';
 import { AppContext } from './context/AppContext';
+import { useState } from 'react';
 
 import ProductItemCard from './ProductItemCard';
 
 function ProductList() {
    const { state } = useContext(AppContext);
    const productState = state.startProducts && state?.startProducts;
+   const [searchProduct, setSearchProduct] = useState('');
+
+   // function searchProductItems() {
+   //    return productState
+   //       .filter((item: any) => {
+   //          if (searchProduct === '') {
+   //             return item;
+   //          } else if (
+   //             item.title
+   //                .toLowerCase()
+   //                .includes(searchProduct.toLocaleLowerCase())
+   //          ) {
+   //             return item;
+   //          }
+   //       })
+   //       .map((item: ProductItem) => <ProductItemCard key={item.id} {...item} />);
+   // }
+
+   console.log(searchProduct);
 
    return (
       <Wrapper>
-         {productState.map((item: ProductItem) => (
-            <ProductItemCard key={item.id} {...item} />
-         ))}
+         <div className="searchbar">
+            <input
+               onChange={(e) => setSearchProduct(e.target.value)}
+               value={searchProduct}
+               placeholder="Search"
+               className="search"
+               type="text"
+            />
+         </div>
+         <div className="test">
+            {productState
+               .filter((item: any) => {
+                  if (searchProduct === '') {
+                     return item;
+                  } else if (
+                     item.title.toLowerCase().includes(searchProduct.toLowerCase())
+                  ) {
+                     return item;
+                  }
+               })
+               .map((item: ProductItem) => (
+                  <ProductItemCard key={item.id} {...item} />
+               ))}
+         </div>
       </Wrapper>
    );
 }
 
 const Wrapper = styled.div`
-   display: grid;
-   grid-template-rows: 340px 340px 340px;
-   grid-template-columns: 300px 300px 300px;
-   justify-content: center;
-   align-content: center;
-   gap: 3rem;
-   margin-top: 3rem;
-   padding-top: 1rem;
+   .test {
+      display: grid;
+      grid-template-rows: 340px 340px 340px;
+      grid-template-columns: 300px 300px 300px;
+      justify-content: center;
+      align-content: center;
+      gap: 3rem;
+      margin-top: 3rem;
+      padding-top: 1rem;
+   }
+
+   .searchbar {
+      display: flex;
+      justify-content: center;
+   }
+
+   input {
+      align-items: center;
+      color: #160427;
+      width: 165px;
+      height: 35px;
+   }
 `;
 
 export default ProductList;
